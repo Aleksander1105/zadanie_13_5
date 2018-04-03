a77a9a537d6d06781730c8b390b066be51348ab8
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-04-03 14:16:49 +0200

    Init project

---
+++ index.js.js
@@ -0,0 +1,28 @@
+var OSinfo = require('./OSinfo.js');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if(input !== null) {
+		var instruction = input.toString().trim();
+		switch (instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/version':
+				console.log(process.versions);
+				break;
+			case '/env':
+				process.stdout.write('Process language is as following: ' + process.env.LANG + ' \n');
+				break;
+			case '/getOSinfo':
+				OSinfo.print();
+				break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		} 
+	}
+	 
+})
+
